3389c73d248919a747ff03d4e048633e12a9ee0d
shaunak <shaunakondare44@gmail.com>
2026-04-10 13:26:47 +0530

    initialize terraform provider and s3 backend configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "YOUR_BUCKET_NAME" # Change this
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "YOUR_DYNAMODB_TABLE_NAME" # Change this
-    encrypt        = true
-  }
 }
 
 provider "aws" {
